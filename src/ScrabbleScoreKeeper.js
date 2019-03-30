c8c14103015033643a30060994e2f33a68a0ba88
Anna Maniuk <anna@maniuk.nyc>
2019-03-29 20:56:04 -0400

    Take out stuff

--- src/ScrabbleScoreKeeper.js
+++ src/ScrabbleScoreKeeper.js
@@ -4,94 +4,11 @@ import ScrabbleInputBox from './ScrabbleInputBox.js';
 import ScrabbleTile from './ScrabbleTile.js';
 import {resizeArray} from './Util.js';
 import Tooltip from './Tooltip.js';
+import Game from './game.js';
+import {scrabbleScore} from './Util.js';
 
 const debug = true;
 let old_game;
-
-class Turn {
-  constructor(words, bingo) {
-    this.words = words;
-    this.bingo = bingo
-  }
-
-  static empty() {
-    return new Turn([], false);
-  }
-
-  get score() {
-    let result = 0;
-    for (let i = 0; i < this.words.length; i++) {
-      result += this.words[i].score
-      }
-
-    if (this.bingo) {
-      result += 50;
-    }
-    return result
-  }
-}
-
-class Game {
-  constructor(players, currentPlayerIndex) {
-    this.currentPlayerIndex = currentPlayerIndex;
-    this.players = players;
-  }
-
-  static createNewGame(numberOfPlayers) {
-    let turn = Turn.empty()
-    let players = resizeArray([[turn]], numberOfPlayers, []);
-    return new Game(players, 0);
-  }
-
-  addWord(word) {
-    let currentTurn = this._getCurrentTurn()
-    let turn = new Turn([...currentTurn.words, word], currentTurn.bingo)
-    return this._setTurn(turn)
-  }
-
-  endTurn(word) {
-    let newPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length
-    let players = this.players.map((history, playerIndex) => playerIndex === newPlayerIndex ? [...history, Turn.empty()] : history)
-    return new Game(players, newPlayerIndex);
-  }
-
-  setBingo(value) {
-    let turn = new Turn(this._getCurrentTurn().words, value)
-    return this._setTurn(turn)
-  }
-
-  _setTurn(turn) {
-    let currentPlayerCopy = this._getCurrentPlayer().slice();
-    currentPlayerCopy[this.getCurrentTurnNumber()] = turn;
-    let newPlayers = this.players.map((player, playerIndex) => playerIndex === this.currentPlayerIndex ? currentPlayerCopy : player);
-    return new Game (newPlayers, this.currentPlayerIndex)
-  }
-
-  _getCurrentTurn() {
-    return this._getCurrentPlayer().slice(-1)[0];
-  }
-
-  _getCurrentPlayer() {
-    return this.players[this.currentPlayerIndex];
-  }
-
-  getCurrentTurnNumber() {
-    return this.players[0].length - 1
-  }
-
-  getCurrentPlayerIndex() {
-    return this.currentPlayerIndex;
-  }
-  
-  getTotalScore(playerIndex) {
-    let currentPlayer = this.players[playerIndex]
-    let result = 0
-    for (let i = 0; i < (currentPlayer.length); i++) {
-        result += currentPlayer[i].score
-    }
-    return result
-  };
-}
 
 class ScoreKeeper extends React.Component {
   constructor(props) {
