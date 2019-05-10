19df35f714172babfd434d3adb0399c3363757f8
Anna Maniuk <anna@maniuk.nyc>
2019-05-10 20:32:20 +0200

    Refactoring application structure.Added a css file for every component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ScrabbleScoreKeeper from './ScrabbleScoreKeeper.js';
+import ScrabbleScoreKeeper from './components/ScrabbleScoreKeeper.js';
 import * as serviceWorker from './serviceWorker';
 
 
