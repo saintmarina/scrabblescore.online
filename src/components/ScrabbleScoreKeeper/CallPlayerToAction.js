b6ba011113f89ce0fb597dda381956217dedbe08
Anna Maniuk <anna@maniuk.nyc>
2019-11-25 15:00:49 -0500

    Test

--- src/components/ScrabbleScoreKeeper/CallPlayerToAction.js
+++ src/components/ScrabbleScoreKeeper/CallPlayerToAction.js
@@ -17,6 +17,7 @@ function CallPlayerToAction(props) {
   return (
     <div className="row justify-content-center">
       <span className="call-player-to-action">{callPlayerToAction()}</span>
+      <div> Test </div>
     </div>
   );
 }
