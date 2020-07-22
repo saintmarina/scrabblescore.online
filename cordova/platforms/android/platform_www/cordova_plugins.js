a479fb3c30dd4820ec0e84ff805ef154690e730f
Anna Maniuk <anna@maniuk.nyc>
2020-07-21 22:43:27 -0400

    Final commit for July 2020. Cordova build updates

--- cordova/platforms/android/platform_www/cordova_plugins.js
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -7,10 +7,19 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "Ionic.WebView"
       ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-ionic-webview": "4.1.3",
+    "cordova-plugin-statusbar": "2.4.3",
     "cordova-plugin-whitelist": "1.3.4"
   };
 });
