211ea763c0586c9986ae2e3c92733042ce18c74d
Adam Royle <adam@ifunk.net>
2023-06-30 19:06:33 +1000

    Deploy to superpower.dev

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,11 @@ rm -rf build
 npm run build
 
 cd build
-echo scrabblescore.online > CNAME
-echo www.scrabblescore.online >> CNAME
+echo scrabblescore.superpower.dev > CNAME
 git init
 git add .
 git commit -am 'Deploy'
-git push -f https://github.com/saintmarina/scrabblescore.online master:gh-pages
+git push -f https://github.com/adamroyle/scrabblescore.online master:gh-pages
 
 # To deploy in Cordova Android:
 # open Android Studio, goto assets/index.js change all paths from Absolute to Relative.
